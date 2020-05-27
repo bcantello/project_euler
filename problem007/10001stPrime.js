a743032dae3bf37f0f9949e4994738b465de5cf5
Brandon Cantello <bcantello@gmail.com>
2020-05-27 11:20:14 -0700

    adds solution for problem 7

---
+++ problem007/10001stPrime.js
@@ -0,0 +1,26 @@
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//
+// What is the 10,001st prime number?
+
+function isPrime(n) {
+	if (n<2) return false;
+	for (let i = 2; i <= Math.sqrt(n); i++)
+		if (n % i === 0) return false;
+	return true;
+}
+
+function nthPrimeNumber(n) {
+	let count = 0;
+	num = 2
+	while (count < n) {
+		if (isPrime(num) === true) {
+			count++;
+			num++;
+		} else {
+			num++
+		}
+	}
+	return num - 1;
+}
+
+console.log(nthPrimeNumber(10001));
